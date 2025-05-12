a6e149104560676c87c307978e20b18455d13488
Bran van der Meer <bran.van.der.meer@protonmail.com>
2025-05-12 22:14:12 +0200

    test: Fix missing Provider for cypress component tests

--- cypress/support/mock-context.tsx
+++ cypress/support/mock-context.tsx
@@ -1,8 +1,13 @@
 import { ReactElement } from 'react'
 import { HashRouter } from 'react-router-dom'
+import { Provider } from '../../src/ui/provider'
 
 export function MockContext(props: {
   children: ReactElement
 }) {
-  return <HashRouter>{props.children}</HashRouter>
+  return (
+    <Provider>
+      <HashRouter>{props.children}</HashRouter>
+    </Provider>
+  )
 }
